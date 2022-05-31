f172388b36cbdbff9678458389a8c17cd068a5d4
Jason Sweeney <jasonsweeney@Jasons-MacBook-Pro-2.local>
2022-05-31 15:35:09 +0100

    Update version

--- src/OnTheFlyIntegration.php
+++ src/OnTheFlyIntegration.php
@@ -22,7 +22,7 @@ use yii\base\Event;
 class OnTheFlyIntegration extends Plugin
 {
     public static $plugin;
-    public $schemaVersion = '1.0.5';
+    public $schemaVersion = '1.0.6';
     public $hasCpSettings = true;
     public $hasCpSection = false;
 
